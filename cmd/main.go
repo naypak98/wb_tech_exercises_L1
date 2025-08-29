4f91394507656339c92d9b88075d51ea8663c53f
Anastasiia <a.naypak@gmail.com>
2025-08-29 11:17:24 +0400

    l1_1 completed

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"wb_tech_exercises_L1/tasks/l1_1"
+)
+
+func main() {
+	fmt.Println("Run l1_1 exercise")
+	l1_1.Run()
+
+}
---
+++ tasks/l1_1/l1_1.go
@@ -0,0 +1,31 @@
+package l1_1
+
+import "fmt"
+
+type Human struct {
+	Name    string
+	Surname string
+	Age     int
+}
+
+func (h Human) Student() string {
+	return fmt.Sprintf("Студент \"%s %s\" (возраст %d)", h.Surname, h.Name, h.Age)
+}
+
+type Action struct {
+	Human
+	ActionName string
+}
+
+func Run() {
+	a := Action{
+		Human: Human{
+			Name:    "Алиса",
+			Surname: "Попова",
+			Age:     22,
+		},
+		ActionName: "зачислен",
+	}
+
+	fmt.Println(a.Student() + " " + a.ActionName)
+}
